fb3329e551698d1cc2b35904c7961ef2e67f6e65
Jang Eunhye <hh961130@naver.com>
2026-01-13 16:14:36 +0900

    design: 산책페이지 컨텐츠 구조

--- App.tsx
+++ App.tsx
@@ -3,15 +3,18 @@ import { StatusBar } from "expo-status-bar";
 import { QueryProvider } from "./providers/QueryProvider";
 import TabNavigator from "./components/TabNavigation";
 import { View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <QueryProvider>
-      <View className="flex-1">
-        <TabNavigator />
-        {/* 상태바 설정 */}
-        <StatusBar style="auto" />
-      </View>
-    </QueryProvider>
+    <SafeAreaProvider>
+      <QueryProvider>
+        <SafeAreaView edges={["top"]} className="flex-1">
+          <TabNavigator />
+          {/* 상태바 설정 */}
+          <StatusBar style="auto" />
+        </SafeAreaView>
+      </QueryProvider>
+    </SafeAreaProvider>
   );
 }
--- pages/WalkPage.tsx
+++ pages/WalkPage.tsx
@@ -2,17 +2,33 @@ import { View, Text, TouchableOpacity } from "react-native";
 
 export default function WalkPage() {
   return (
-    <View className="flex-1  items-center justify-center">
+    <View className="flex-1 bg-white">
       <View className="flex-1">
-        <Text className="text-2xl font-bold text-gray-800">
-          2026년 1월 첫째주
-        </Text>
-        <Text className="text-2xl font-bold text-gray-800">ㅇㅇㅇㅇㅇㅇㅇ</Text>
+        {/*날짜 영역*/}
+        <View className="flex-1 bg-red-200 justify-center">
+          <Text className="text-2xl font-bold text-gray-800">
+            2026년 1월 첫째주
+          </Text>
+        </View>
+
+        {/*이번주 산책 현황*/}
+        <View className="flex-[2] bg-yellow-200 items-center justify-center">
+          <Text className="text-2xl font-bold text-gray-800">
+            ㅇㅇㅇㅇㅇㅇㅇ
+          </Text>
+        </View>
       </View>
-      <View className="flex[4 flex-row">
+
+      {/*아이콘 영역*/}
+      <View className="flex-[4] bg-green-200 items-center justify-center">
         <Text className="text-2xl font-bold text-gray-800">아이콘</Text>
       </View>
-      <TouchableOpacity className="flex-1" onPress={() => {}}>
+
+      {/*산책 시작/종료 버튼*/}
+      <TouchableOpacity
+        className="flex-1 bg-blue-200 items-center justify-center"
+        onPress={() => {}}
+      >
         <Text className="text-2xl font-bold text-gray-800">산책 시작</Text>
       </TouchableOpacity>
     </View>
